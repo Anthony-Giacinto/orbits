ddcd7c35cc29b8cc76c0e6286facd0ff0b9cf9f2
Anthony-Giacinto <anthonygiacinto1@gmail.com>
2020-12-17 23:37:36 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(name='orbits_GUI',
+      version='1.0',
+      description='Allows the user to animate the trajectories of objects in space',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/Anthony-Giacinto/orbits_gui',
+      author='Anthony Giacinto',
+      author_email='anthonygiacinto1@gmail.com',
+      packages=find_packages())
